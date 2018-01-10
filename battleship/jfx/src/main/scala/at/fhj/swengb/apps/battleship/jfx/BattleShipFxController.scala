20d10c88cbf49351f2fd02ba049159251d9891c9
VeeLeifert <verena.leifert@edu.fh-joanneum.at>
2018-01-10 12:20:39 +0100

    Using Stefans and Michis Battleship HW for Projectbase + first Project Report added

--- battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipFxController.scala
+++ battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipFxController.scala
@@ -1,65 +1,130 @@
 package at.fhj.swengb.apps.battleship.jfx
 
+import java.io.File
 import java.net.URL
+import java.nio.file.{Files, Paths}
 import java.util.ResourceBundle
 import javafx.fxml.{FXML, Initializable}
-import javafx.scene.control.TextArea
+import javafx.scene.control.{TextArea, Slider, Label}
 import javafx.scene.layout.GridPane
+import javafx.stage.FileChooser
+import javafx.stage.FileChooser.ExtensionFilter
 
-import at.fhj.swengb.apps.battleship.model.{BattleField, BattleShipGame, Fleet, FleetConfig}
-
+import at.fhj.swengb.apps.battleship.model.{BattleField, BattleShipGame, Fleet, FleetConfig, BattlePos}
+import at.fhj.swengb.apps.battleship.BattleShipProtocol
 
 class BattleShipFxController extends Initializable {
 
+  private var Game: BattleShipGame = _
+
+  override def initialize(url: URL, rb: ResourceBundle): Unit = newGame()
+  def LogAdder3000(text: String): Unit = log.appendText(text + "\n")
+  def WidthReader3000(width: Int): Int = battleGroundGridPane.getColumnConstraints.get(width).getPrefWidth.toInt
+  def HeightReader3000(height: Int): Int = battleGroundGridPane.getRowConstraints.get(height).getPrefHeight.toInt
+
+
+
+  def Initiator3000(game: BattleShipGame, ClickChecker3000: List[BattlePos]): Unit = {
+    Game = game
+    battleGroundGridPane.getChildren.clear()
+    for (cells <- game.CellReader3000()) {
+
+      battleGroundGridPane.add(cells, cells.pos.x, cells.pos.y)
+    }
+    game.GameState = List()
+    game.CellReader3000().foreach(c => c.init())
+    game.RebuildGame(ClickChecker3000)
+    SliderAdder3000(ClickChecker3000.size)
+  }
+
+
+  private def GameCreator3000(): BattleShipGame = {
+    val Field = BattleField(10, 10, Fleet(FleetConfig.Standard))
+    BattleShipGame(BattleField.RandomPlacer3000(Field), LogAdder3000, SliderAdder3000, WidthReader3000, HeightReader3000)
+  }
+
+  private def GameLoader3000(filePath: String): (BattleShipGame, List[BattlePos]) = {
+    val LoadDestination = at.fhj.swengb.apps.battleship.BattleShipProtobuf.BattleShipGame
+        .parseFrom(Files.newInputStream(Paths.get(filePath)))
+
+    val Game = BattleShipGame(BattleShipProtocol.convert(LoadDestination).battleField,
+      LogAdder3000,
+      SliderAdder3000,
+      WidthReader3000,
+      HeightReader3000)
+
+    Game.GameState = List()
+    (Game, BattleShipProtocol.convert(LoadDestination).GameState.reverse)
+  }
+
+  def SliderAdder3000(Stack: Int): Unit = {
+    SliderState.setMax(Stack)
+    SliderState.setValue(Stack)
+  }
 
   @FXML private var battleGroundGridPane: GridPane = _
+  @FXML private var SliderState: Slider = _
+  @FXML private var Title: Label = _
 
-  /**
-    * A text area box to place the history of the game
-    */
   @FXML private var log: TextArea = _
 
-  @FXML
-  def newGame(): Unit = initGame()
+  //Creating a new game and resetting all states
+  @FXML def newGame(): Unit = {
+    log.setText("")
+    LogAdder3000("A new game has started")
+    Initiator3000(GameCreator3000(), List())
+  }
 
-  override def initialize(url: URL, rb: ResourceBundle): Unit = initGame()
+  @FXML def saveGame(): Unit = {
+      //Using FileChooser for accessing our files
+      val FileChooser3000 = new FileChooser();
+      //Filtering on our protobuf files with the ending .bin
+      val ProtoFilter3000: FileChooser.ExtensionFilter = new ExtensionFilter("Protobuf files","*.bin")
+      FileChooser3000.getExtensionFilters.add(ProtoFilter3000)
+      //Converting and saving
+      val FileSaver3000: File = FileChooser3000.showSaveDialog(BattleShipFxApp.FirstStage3000)
+      BattleShipProtocol.convert(Game).writeTo(Files.newOutputStream(Paths.get(FileSaver3000.getAbsolutePath)))
+      LogAdder3000("Saved Game")
+  }
 
-  private def getCellHeight(y: Int): Double = battleGroundGridPane.getRowConstraints.get(y).getPrefHeight
 
-  private def getCellWidth(x: Int): Double = battleGroundGridPane.getColumnConstraints.get(x).getPrefWidth
+  @FXML def loadGame(): Unit = {
+      val FileChooser3000 = new FileChooser();
+      val ProtoFilter3000: FileChooser.ExtensionFilter = new ExtensionFilter("Protobuf files","*.bin")
+      FileChooser3000.getExtensionFilters.add(ProtoFilter3000)
+      val FileLoader3000: File = FileChooser3000.showOpenDialog(BattleShipFxApp.FirstStage3000)
+      val (clickedPos, battleShipGame) = GameLoader3000(FileLoader3000.getAbsolutePath)
+      //Resetting log
+      log.setText("")
+      Initiator3000(clickedPos, battleShipGame)
+      LogAdder3000("Loaded Game")
+  }
 
-  def appendLog(message: String): Unit = log.appendText(message + "\n")
+  @FXML def onSliderChanged(): Unit = {
+    val TimeOnSlider = SliderState.getValue.toInt
+    var PastChecker: Boolean = true
+    val PastState: List[BattlePos] = Game.GameState.takeRight(TimeOnSlider).reverse
 
-  /**
-    * Create a new game.
-    *
-    * This means
-    *
-    * - resetting all cells to 'empty' state
-    * - placing your ships at random on the battleground
-    *
-    */
-  def init(game : BattleShipGame) : Unit = {
-    battleGroundGridPane.getChildren.clear()
-    for (c <- game.getCells) {
-      battleGroundGridPane.add(c, c.pos.x, c.pos.y)
+    if (TimeOnSlider != SliderState.getMax.toInt) {
+      log.setText("")
+      Title.setText("Reviewing Past")
+      PastChecker = true
+      LogAdder3000("Reviewing Past")
+    } else {
+      log.setText("")
+      Title.setText("BATTLESHIP")
+      PastChecker = false
+      Game.GameState = List()
+      LogAdder3000("Back to Present")
     }
-    game.getCells().foreach(c => c.init)
+    battleGroundGridPane.getChildren.clear()
+    for (cells <- Game.CellReader3000()) {
+      battleGroundGridPane.add(cells, cells.pos.x, cells.pos.y)
+      cells.init()
+      cells.setDisable(PastChecker) //PastChecker True if past -> cells deactivated
+    }
+    Game.RebuildGame(PastState) //Rebuilding the Game
   }
 
-
-  private def initGame(): Unit = {
-    val game: BattleShipGame = createGame()
-    init(game)
-    appendLog("New game started.")
-  }
-
-  private def createGame(): BattleShipGame = {
-    val field = BattleField(10, 10, Fleet(FleetConfig.Standard))
-
-    val battleField: BattleField = BattleField.placeRandomly(field)
-
-    BattleShipGame(battleField, getCellWidth, getCellHeight, appendLog)
-  }
 
 }
--- battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/model/BattleField.scala
+++ battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/model/BattleField.scala
@@ -2,74 +2,52 @@ package at.fhj.swengb.apps.battleship.model
 
 import scala.util.Random
 
-object BattleField {
-
-  def placeRandomly(bf: BattleField): BattleField = {
-
-    def loop(vesselsToPlace: Set[Vessel], workingBattleField: BattleField): BattleField = {
-      if (vesselsToPlace.isEmpty) workingBattleField
-      else {
-        val v = vesselsToPlace.head
-        loop(vesselsToPlace.tail, workingBattleField.addAtRandomPosition(v))
-      }
-
-    }
-
-    loop(bf.fleet.vessels, bf.copy(fleet = bf.fleet.copy(vessels = Set())))
-  }
-
-}
-
 /**
   * Denotes the size of our region of interest
   */
 case class BattleField(width: Int, height: Int, fleet: Fleet) {
+  val Positions: Set[BattlePos] = (for {x <- 0 until width
+                                        y <- 0 until height} yield BattlePos(x, y)).toSet
+  val availablePos: Set[BattlePos] = Positions -- fleet.occupiedPositions
 
   /**
     * Adds vessel at a random, free position in the battlefield. if no position could be found,
     * returns the current battlefield without vessel added.
     *
-    * @param v vessel to add
+    * @param Vessel3000 vessel to add
     * @return
     */
-  def addAtRandomPosition(v: Vessel): BattleField = {
+  def addAtRandomPosition(Vessel3000: Vessel): BattleField = {
 
-    def loop(pos: Set[BattlePos], currBf: BattleField, found: Boolean): BattleField = {
-      if (found) {
-        println(s"Placed vessel of type ${v.getClass.getSimpleName} on battlefield ...")
-        currBf
-      } else if (pos.isEmpty) {
-        println(s"Giving up on vessel of type ${v.getClass.getSimpleName}. No place left.")
-        currBf
-      } else {
-        // take random position out of available positions
-        val p = pos.toSeq(Random.nextInt(pos.size))
-        val vessel = v.copy(startPos = p)
-        if (vessel.occupiedPos.subsetOf(availablePos)) {
-          loop(pos - p, currBf.copy(fleet = currBf.fleet.copy(vessels = currBf.fleet.vessels + vessel)), true)
+    def Iterator3000(Placed: Boolean, Position: Set[BattlePos], Field: BattleField): BattleField = {
+      if (Placed == false) {
+        val p = Position.toSeq(Random.nextInt(Position.size))
+        if (Vessel3000.copy(InitPosition = p).occupiedPos.subsetOf(availablePos)) {
+          Iterator3000(true, Position - p, Field.copy(fleet = Field.fleet.copy(vessels = Field.fleet.vessels + Vessel3000.copy(InitPosition = p))))
         } else {
-          loop(pos - p, currBf, false)
+          Iterator3000(false, Position - p, Field)
         }
+      } else if (Position.isEmpty) {
+        println(s"Can't place ${Vessel3000.getClass.getSimpleName} on the battlefield!")
+        Field
+      } else {
+        println(s"Placed ${Vessel3000.getClass.getSimpleName} on the battlefield!")
+        Field
       }
     }
-
-    loop(availablePos, this, false)
+    Iterator3000(false, availablePos, this)
 
   }
+}
 
-
-  /**
-    * All positions in this battlefield
-    */
-  val allPos: Set[BattlePos] = (for {x <- 0 until width
-                                     y <- 0 until height} yield BattlePos(x, y)).toSet
-
-
-  val availablePos: Set[BattlePos] = allPos -- fleet.occupiedPositions
-
-  def randomFleet(): Fleet = {
-    Fleet(Set[Vessel]())
+object BattleField {
+  def RandomPlacer3000(BattleField3000: BattleField): BattleField = {
+    def Placer3000(CurrentBattleField: BattleField, Vessels: Set[Vessel]): BattleField = {
+      if (Vessels.isEmpty) CurrentBattleField
+      else {
+        Placer3000(CurrentBattleField.addAtRandomPosition(Vessels.head), Vessels.tail)
+      }
+    }
+    Placer3000(BattleField3000.copy(fleet = BattleField3000.fleet.copy(vessels = Set())), BattleField3000.fleet.vessels)
   }
-
-
 }
